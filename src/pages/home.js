774c73f8f1ee9ad97dfce340a41f21eddb284dee
natalie <wong.lt.natalie@gmail.com>
2025-04-19 21:35:31 -0400

    Add heading to article

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,4 +2,15 @@ export default function Home() {
     // Set page title
     const title = document.querySelector('title');
     title.innerText = 'Natalie Wong';
+
+    const article = document.querySelector('article');
+
+    // Header
+    const heading = document.createElement('h1');
+    heading.setAttribute('id', 'home-heading');
+    const headingSpan = document.createElement('span');
+    heading.innerText = 'Hey, I\'m Natalie, \nA ';
+    headingSpan.innerText = 'Web Developer.';
+    heading.appendChild(headingSpan);
+    article.appendChild(heading);
 }
