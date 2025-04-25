136d7eb666769219087c77d2e5f620f267c0e6e5
unknown <natalieltwong@gmail.com>
2025-04-24 22:42:04 -0400

    Add project background images

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,9 +6,9 @@ project1.setAttribute('id', 'project1');
 const project1Desc = document.createElement('div');
 project1Desc.setAttribute('class', 'project-desc');
 const project1Title = document.createElement('h3');
-project1Title.innerText = 'Project 1';
+project1Title.innerText = 'Calculator';
 const project1DescText = document.createElement('p');
-project1DescText.innerText = 'This is a description of project 1.';
+project1DescText.innerText = 'This is a description of project 1.'; // edit
 project1Desc.appendChild(project1Title);
 project1Desc.appendChild(project1DescText);
 project1.appendChild(project1Desc);
@@ -18,33 +18,31 @@ project2.setAttribute('id', 'project2');
 const project2Desc = document.createElement('div');
 project2Desc.setAttribute('class', 'project-desc');
 const project2Title = document.createElement('h3');
-project2Title.innerText = 'Project 2';
+project2Title.innerText = 'Etch-a-Sketch';
 const project2DescText = document.createElement('p');
-project2DescText.innerText = 'This is a description of project 2.';
+project2DescText.innerText = 'This is a description of project 2.'; // edit
 project2Desc.appendChild(project2Title);
 project2Desc.appendChild(project2DescText);
 project2.appendChild(project2Desc);
 
 const project3 = document.createElement('div');
-const project4 = document.createElement('div');
-const project5 = document.createElement('div');
-const project6 = document.createElement('div');
-const project7 = document.createElement('div');
+project3.setAttribute('id', 'project3');
+const project3Desc = document.createElement('div');
+project3Desc.setAttribute('class', 'project-desc');
+const project3Title = document.createElement('h3');
+project3Title.innerText = 'Blinky-ON!';
+const project3DescText = document.createElement('p');
+project3DescText.innerText = 'This is a description of project 3.'; // edit 
+project3Desc.appendChild(project3Title);
+project3Desc.appendChild(project3DescText);
+project3.appendChild(project3Desc);
 
 project1.setAttribute('class', 'project-card');
 project2.setAttribute('class', 'project-card');
 project3.setAttribute('class', 'project-card');
-project4.setAttribute('class', 'project-card');
-project5.setAttribute('class', 'project-card');
-project6.setAttribute('class', 'project-card');
-project7.setAttribute('class', 'project-card');
 
 projectsContainer.appendChild(project1);
 projectsContainer.appendChild(project2);
 projectsContainer.appendChild(project3);
-projectsContainer.appendChild(project4);
-projectsContainer.appendChild(project5);
-projectsContainer.appendChild(project6);
-projectsContainer.appendChild(project7);
 
 export default projectsContainer;
