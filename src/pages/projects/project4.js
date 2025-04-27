5121a962c541245f19a231752e72e2a3feb69d56
Natalie Wong <wong.lt.natalie@gmail.com>
2025-04-27 09:48:52 -0400

    Add project 4 card

---
+++ src/pages/projects/project4.js
@@ -0,0 +1,9 @@
+export default function project4() {
+    // Set page title
+
+
+    const heading = document.createElement('h1');
+    heading.innerText = 'Project 4 - Natalie Wong';
+    
+    content.appendChild(heading);
+}
